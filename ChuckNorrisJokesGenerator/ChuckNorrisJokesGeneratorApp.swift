9913f43051d244230de4704f9ce9db4ccbe16d91
PallArnoldBarna <pallarnoldbarna@gmail.com>
2026-03-10 15:16:22 +0200

    Initial Commit

---
+++ ChuckNorrisJokesGenerator/ChuckNorrisJokesGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisJokesGeneratorApp.swift
+//  ChuckNorrisJokesGenerator
+//
+//  Created by Pall Arnold Barna on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisJokesGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
